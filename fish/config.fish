5da4661673d1c8acc66b8ea0af326c73b3c3036e
SkyDogzz <thomas.stephan@live.fr>
2024-08-10 16:15:19 +0200

    Add starship config

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,25 @@
-alias nvimf="nvim (fzf)"
+starship init fish | source
+
+# General Purpose Aliases
+alias ll="ls -lh"
+alias la="ls -lha"
+alias gs="git status"
+alias gcm="git commit -m"
+alias gpu="git push"
+alias gpom="git push origin master"
+alias dcup="docker-compose up"
+alias dcdown="docker-compose down"
+alias cls="clear"
+alias ..="cd .."
+alias ...="cd ../.."
+alias v="vim"
+alias e="exit"
+alias c="clear"
+
+# Advanced Aliases
+alias mkcd="function mkcd; mkdir -p \$argv; and cd \$argv; end;"
+alias histg="history | grep"
+alias update="sudo apt update && sudo apt upgrade"
+alias ff="find . -type f -name"
+alias weather="curl wttr.in"
+
