4c0ef909d8848706a21acd2acb2182420b450652
SkyDogzz <thomas.stephan@live.fr>
2024-08-15 20:47:42 +0200

    Add more lsp config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -33,4 +33,51 @@ return {
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
   },
+  {
+    "hrsh7th/nvim-cmp",
+    lazy = false,
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+          ['<C-space>'] = cmp.mapping.complete(),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+          { name = 'buffer' },
+          { name = 'path' },
+        }),
+      })
+    end,
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    lazy = false,
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
+  },
+  {
+    "saadparwaiz1/cmp_luasnip",
+    lazy = false,
+  },
+  {
+    "hrsh7th/cmp-nvim-lsp",
+    lazy = false,
+  },
+  {
+    "hrsh7th/cmp-buffer",
+    lazy = false,
+  },
+  {
+    "hrsh7th/cmp-path",
+    lazy = false,
+  },
 }
