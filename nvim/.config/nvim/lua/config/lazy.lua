cd5d9b529d2d42f0f4432f08aeafc9b3854ba130
Skydogzz <tstephan00@outlook.com>
2026-06-06 03:48:19 +0200

    Use system clipboard in Neovim

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -22,6 +22,7 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.clipboard = "unnamedplus"
 
 -- Setup lazy.nvim
 require("lazy").setup({
