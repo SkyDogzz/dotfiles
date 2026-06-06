c5a3d4f78b06adf1e6c847742289332079eb1404
Skydogzz <tstephan00@outlook.com>
2026-06-06 18:04:34 +0200

    feat(nvim): add which-key, comment, indent-blankline, noice, and nvim-lint

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -70,4 +70,24 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        c = { "clang-tidy", "cppcheck" },
+        cpp = { "clang-tidy", "cppcheck" },
+      },
+    },
+    config = function(_, opts)
+      local lint = require("lint")
+
+      lint.linters_by_ft = opts.linters_by_ft
+
+      vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
 }
---
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      lsp = {
+        progress = { enabled = true },
+        override = {
+          "vim.lsp.protocol.begin",
+          true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+      },
+    },
+  },
+}
