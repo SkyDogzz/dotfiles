176aef0af5bb71091173f4ab6dad61028eb712a6
Skydogzz <tstephan00@outlook.com>
2026-06-06 17:31:07 +0200

    feat(nvim): add telescope with fzf-native and ripgrep

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        enabled = vim.fn.executable("make") == 1,
+      },
+    },
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Live grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Help tags" },
+      { "<leader>fs", "<cmd>Telescope grep_string<cr>", desc = "Grep string" },
+      { "<leader>sk", "<cmd>Telescope keymaps<cr>", desc = "Search keymaps" },
+    },
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+        file_ignore_patterns = { "node_modules", ".git", "build", "dist" },
+      },
+      pickers = {
+        find_files = { hidden = true },
+        live_grep = { additional_args = { "--hidden", "--no-ignore" } },
+      },
+    },
+    config = function(_, opts)
+      local telescope = require("telescope")
+      telescope.setup(opts)
+      telescope.load_extension("fzf")
+    end,
+  },
+}
