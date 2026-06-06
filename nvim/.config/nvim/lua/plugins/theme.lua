fc67e36b8d9347ddda9014b98f3ddaa3fc1fd0f2
Skydogzz <tstephan00@outlook.com>
2026-06-06 02:16:16 +0200

    Tweak terminal and nvim transparency

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,1 +1,29 @@
-return { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  opts = {
+    transparent_background = true,
+  },
+  config = function(_, opts)
+    require("catppuccin").setup(opts)
+    vim.cmd.colorscheme("catppuccin")
+
+    local transparent_groups = {
+      "Normal",
+      "NormalNC",
+      "NormalFloat",
+      "FloatBorder",
+      "EndOfBuffer",
+      "SignColumn",
+      "FoldColumn",
+      "MsgArea",
+      "Pmenu",
+      "PmenuSel",
+    }
+
+    for _, group in ipairs(transparent_groups) do
+      vim.api.nvim_set_hl(0, group, { bg = "NONE" })
+    end
+  end,
+}
