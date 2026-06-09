8165c63b1799196468a1f1358e17ff739627b335
Skydogzz <tstephan00@outlook.com>
2026-06-09 17:13:19 +0200

    fix(nvim): define leader before keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,2 +1,14 @@
-vim.keymap.set("n", "ww", "<C-w>w", { desc = "Next window" })
-vim.keymap.set("n", "wW", "<C-w>W", { desc = "Previous window" })
+vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Window left" })
+vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Window down" })
+vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Window up" })
+vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Window right" })
+vim.keymap.set("n", "<leader>ww", "<C-w>w", { desc = "Next window" })
+
+vim.keymap.set("n", "<leader>wv", "<C-w>v", { desc = "Split window vertical" })
+vim.keymap.set("n", "<leader>ws", "<C-w>s", { desc = "Split window horizontal" })
+
+vim.keymap.set("n", "<leader>wc", "<C-w>c", { desc = "Close window" })
+vim.keymap.set("n", "<leader>wo", "<C-w>o", { desc = "Only window" })
+vim.keymap.set("n", "<leader>w=", "<C-w>=", { desc = "Equalize window sizes" })
+vim.keymap.set("n", "<leader>wx", "<C-w>x", { desc = "Exchange window" })
+vim.keymap.set("n", "<leader>wt", "<C-w>T", { desc = "Move window to tab" })
