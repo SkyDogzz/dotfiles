98965ab5041b42a51584a49b2397acdbc51e1ff5
Skydogzz <tstephan00@outlook.com>
2026-06-10 18:00:25 +0200

    fix(nvim): reinstall markdown preview deps

--- nvim/.config/nvim/lua/plugins/markdown-preview.lua
+++ nvim/.config/nvim/lua/plugins/markdown-preview.lua
@@ -1,8 +1,6 @@
 return {
   "iamcco/markdown-preview.nvim",
-  build = function()
-    vim.fn["mkdp#util#install"]()
-  end,
+  build = "cd app && npm install",
   cmd = { "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
   ft = "markdown",
   init = function()
